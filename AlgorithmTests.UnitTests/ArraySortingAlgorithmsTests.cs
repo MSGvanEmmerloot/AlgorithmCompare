995738c835fb3aebe58cf3b014f6ec22b061ab40
Mika van Emmerloot <mika.van.emmerloot@alten.nl>
2019-11-11 08:57:01 +0100

    Initial Commit of solution containing the AlgorithmTests project and the AlgorithmTests.UnitTests project

---
+++ AlgorithmTests.UnitTests/ArraySortingAlgorithmsTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmTests;
+
+namespace AlgorithmTests.UnitTests
+{
+    [TestClass]
+    public class ArraySortingAlgorithmsTests
+    {
+        [TestMethod]
+        public void CheckArraySorted_SortedArrayNoDuplicates_ReturnsTrue()
+        {
+            int[] testArray = new int[] { 0, 1, 2, 3, 4, 5 };
+
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CheckArraySorted_SortedArrayWithDuplicates_ReturnsTrue()
+        {
+            int[] testArray = new int[] { 0, 0, 2, 2, 4, 5 };
+
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CheckArraySorted_UnsortedArrayNoDuplicates_ReturnsFalse()
+        {
+            int[] testArray = new int[] { 0, 1, 2, 3, 5, 4 };
+
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CheckArraySorted_UnsortedArrayWithDuplicates_ReturnsFalse()
+        {
+            int[] testArray = new int[] { 0, 1, 3, 3, 2, 4 };
+
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void BubbleSort_ReturnsSortedArray()
+        {
+            int[] testArray = new int[] { 0, 5, 1, 4, 3, 2 };
+
+            ArraySortingAlgorithms.BubbleSort(testArray);
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SelectionSort_ReturnsSortedArray()
+        {
+            int[] testArray = new int[] { 0, 5, 1, 4, 3, 2 };
+
+            ArraySortingAlgorithms.SelectionSort(testArray);
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void InsertionSort_ReturnsSortedArray()
+        {
+            int[] testArray = new int[] { 0, 5, 1, 4, 3, 2 };
+
+            ArraySortingAlgorithms.InsertionSort(testArray);
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void MergeSort_ReturnsSortedArray()
+        {
+            int[] testArray = new int[] { 0, 5, 1, 4, 3, 2 };
+
+            ArraySortingAlgorithms.MergeSort(testArray);
+            bool result = ArraySortingAlgorithms.CheckArraySorted(testArray);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ AlgorithmTests.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlgorithmTests.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlgorithmTests.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3362666e-e24e-424e-9e06-2f9a58f03979")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
